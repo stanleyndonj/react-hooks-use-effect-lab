28b50fbe3c760feb604f6266ec7a54675e363f9e
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-09-07 13:25:21 +0300

    labcomplete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  useEffect(() => {
 
-  // add useEffect code
+    const timerId = setTimeout(() => {
+      
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+
+    if (timeRemaining === 0) {
+
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+    return () => clearTimeout(timerId);
+
+  
+  }, [timeRemaining, onAnswered]);
+
 
   function handleAnswer(isCorrect) {
+    
     setTimeRemaining(10);
+    
     onAnswered(isCorrect);
   }
 
@@ -16,14 +34,16 @@ function Question({ question, onAnswered }) {
     <>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
+      {/* Render each answer as a button */}
       {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
+        const isCorrect = index === correctIndex; 
         return (
           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
             {answer}
           </button>
         );
       })}
+      {/* Display the remaining time */}
       <h5>{timeRemaining} seconds remaining</h5>
     </>
   );
